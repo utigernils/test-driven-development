04ca704dc2c8a4a42e45bc63d9ae98c8d443d5d4
utigernils <utigernils@gmail.com>
2025-04-10 13:18:00 +0200

    feat; implement "formatDuration" fuction with the help of test driven development

--- src/duration.ts
+++ src/duration.ts
@@ -1,4 +1,25 @@
 export function formatDuration(seconds: number): string {
-  console.log(seconds);
-  throw new Error("not implemented yet");
+  if (seconds < 0) {
+    throw new Error('Duration cannot be negative');
+  }
+
+  seconds = Math.round(seconds);
+
+  const hours = Math.floor(seconds / 3600);
+  seconds %= 3600;
+  const minutes = Math.floor(seconds / 60);
+  seconds %= 60;
+
+  let result = '';
+  if (hours > 0) {
+    result += `${hours}h`;
+  }
+  if (minutes > 0) {
+    result += `${minutes}m`;
+  }
+  if (seconds > 0 || result === '') {
+    result += `${seconds}s`;
+  }
+
+  return result;
 }
